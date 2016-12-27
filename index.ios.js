28a9e1f914fb5039b8df324f44f2daadf16563bf
kwoumn3 <kwoumn3@gatech.edu>
2016-12-27 06:34:35 -0500

    Move firebase code to dedicated api/firebase.js

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,5 @@
 
 import { AppRegistry } from 'react-native';
 import setup from './js/setup';
-import * as firebase from 'firebase';
-
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDscOMPAjF1Q286bt4R3w2IA_6RAlk1RmI",
-  authDomain: "<your-auth-domain>",
-  storageBucket: "gs://project-6478449439913979648.appspot.com",
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 AppRegistry.registerComponent('Khemet', setup);
