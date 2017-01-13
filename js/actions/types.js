c6ecebdfa05bcce3c995c56388b3a0a11e31bd0d
kwoumn3 <kwoumn3@gatech.edu>
2017-01-13 02:45:50 -0500

    Add actions for credentials

--- js/actions/types.js
+++ js/actions/types.js
@@ -8,6 +8,11 @@ export type Action =
     | { type: 'REPLACE_OR_PUSH_ROUTE', route: string, passProps:any }
     | { type: 'OPEN_DRAWER'}
     | { type: 'CLOSE_DRAWER'}
+    | {type: 'ADD_FIRST_NAME', firstName: string}
+    | {type: 'ADD_LAST_NAME', lastName: string}
+    | {type: 'ADD_EMAIL_ADDRESS', emailAddress: string}
+    | {type: 'ADD_PASSWORD', password: string}
+    | {type: 'ADD_ORGANIZATION', organization: string}
 
 export type Dispatch = (action:Action | Array<Action>) => any;
 export type GetState = () => Object;
