5b333255c2178f82249cc3f841c50c4dc7d013f6
kwoumn3 <kwoumn3@gatech.edu>
2017-01-13 02:49:22 -0500

    Subscribe to state changes

--- js/configureStore.js
+++ js/configureStore.js
@@ -18,5 +18,9 @@ export default function configureStore(onCompletion:()=>void):any {
   const store = createStore(reducer, enhancer);
   persistStore(store, { storage: AsyncStorage }, onCompletion);
 
+  let unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+  )
+
   return store;
 }
