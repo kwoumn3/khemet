8cbc3a45907bcd323cf255418e9499961f895f08
kwoumn3 <kwoumn3@gatech.edu>
2017-01-13 02:50:35 -0500

    Combine new credentials reducers with other reducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 
 import drawer from './drawer';
 import cardNavigation from './cardNavigation';
+import {credentials} from './user';
 
 export default combineReducers({
   drawer,
   cardNavigation,
+  credentials
 });
