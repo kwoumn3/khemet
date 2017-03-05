37459afb4f4aab0a802346fb8b001a9000321932
kwoumn3 <kwoumn3@gatech.edu>
2017-03-05 11:11:33 -0800

    Add Initiation Info component

---
+++ js/components/initiationInfo/index.js
@@ -0,0 +1,11 @@
+'use strict';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {View } from 'react-native';
+import { actions } from 'react-native-navigation-redux-helpers';
+
+
+
+class InitiationInfo extends Component {
+
+}
